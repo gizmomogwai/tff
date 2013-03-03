e1d75c6d1d7149b6f09bce26ebcd0fc93fcd386b
Christian Köstlin <christian.koestlin@esrlabs.com>
2013-03-03 09:08:20 +0100

    moved tests to seperate file

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,5 +1,5 @@
 task :default => :test
 
 task :test do
-  sh "/Applications/_system/Emacs.app/Contents/MacOS/Emacs --no-site-file -batch -L . -L #{ENV['HOME']}/.emacs.d/el-get/ert-expectations -l ert-expectations -l tff.el -f batch-expectations"
+  sh "/Applications/_system/Emacs.app/Contents/MacOS/Emacs -Q --no-site-file -batch -L . -L #{ENV['HOME']}/.emacs.d/el-get/ert-expectations -l ert-expectations -l tff-test.el -f batch-expectations"
 end
